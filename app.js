d685dabbeac52a68cfb452970f79dfc16729b0e4
BINQIU1220 <ezio12a@hotmail.com>
2022-05-16 12:57:52 +0100

    task 3 completed and express.json deleted

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const app = express();
 
 const { getAllCatogories } = require("./controllers/games-controller");
 
-app.use(express.json());
-
 // TASK 3
 app.get("/api/categories", getAllCatogories);
 
