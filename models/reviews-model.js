1e5e9e2e3825ddba08a2a6c19fb1d505cfe09087
BINQIU1220 <ezio12a@hotmail.com>
2022-05-20 10:42:08 +0100

    deleted some comments and deleted require checkValsExists in model file

--- models/reviews-model.js
+++ models/reviews-model.js
@@ -1,7 +1,5 @@
 const db = require("../db/connection.js");
 
-const { checkValExists } = require("../db/seeds/utils");
-
 // TASK 4
 exports.fetchReviewById = (id) => {
   return db
@@ -45,13 +43,6 @@ exports.fetchAllReviews = (
     "review_id",
     "comment_count",
   ];
-
-  // const validCategories = [
-  //   "euro game",
-  //   "dexterity",
-  //   "social deduction",
-  //   "children's games",
-  // ];
 
   let queryStr = `SELECT owner, title, reviews.review_id, category,  review_img_url, reviews.created_at, reviews.votes, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON comments.review_id = reviews.review_id`;
 
